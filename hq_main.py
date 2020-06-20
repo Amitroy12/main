4662effbcb7a4ac57b671874f2f917ca674da5d9
Amitroy12 <62181529+Amitroy12@users.noreply.github.com>
2020-06-20 08:50:55 +0530

    Update hq_main.py

--- hq_main.py
+++ hq_main.py
@@ -9,7 +9,7 @@ import networking
 logging.basicConfig(filename="data.log", level=logging.DEBUG, filemode="w")
 
 # Read in bearer token and user ID
-BEARER_TOKEN = "NzAxNDYwOTc0NDkzODkyNzM5.Xpx5PQ.yM8lOt3qSrUcezvHyoYmHYBYWg0"
+BEARER_TOKEN = "NTg0MjQ2NDExOTkwNjYzMjA1.Xu2AJw.UIrKfdcqLnltu4H-unDDMNWeNZ0"
 USER_ID = "701460974493892739"
 
 main_url = "https://api-quiz.hype.space/shows/now?type=hq&userId={USER_ID}"
